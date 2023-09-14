fa60a58a2575d3d061e4aa6970d4483b10fb3c0c
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-09-13 22:39:12 -0300

    feature: adicionando funcao multiplicacao

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def multiplicacao(n1: int, n2: int) -> int:
+    """Realiza a multiplicação dos números
+
+    Args:
+        n1: primeiro número.
+        n2: segundo número.
+
+    Returns:
+        O produto dos números.
+    """
+    return n1 * n2
