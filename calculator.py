e36f56d11233c97693ccb51f539c0c1dd98bb586
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-09-13 22:41:18 -0300

    feature: adicionando função divisão

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def divisao(n1: int, n2: int) -> int:
+    """Realiza a divisão de um número pelo outro
+
+    Args:
+        n1: primeiro número
+        n2: segundo número
+
+    Returns:
+        O quociente dos números
+    """
+    return n1 / n2
+
