04721c788c4d97b01cc06537c1054c5f9965a19f
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-09-13 22:22:46 -0300

    feature: adicionando função adição

--- calculator.py
+++ calculator.py
@@ -1,2 +1,11 @@
-print("teste")
-print("Vamos ver")
+def adicao(n1: int, n2: int) -> int:
+    """Somar dois números
+
+    Args:
+        n1: primeiro número.
+        n2: segundo número.
+
+    Returns:
+        Soma dos números.
+    """
+    return n1 + n2
