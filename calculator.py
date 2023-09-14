8331696418601e244fdeab460b4a7b1bef1fb07a
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-09-13 22:35:25 -0300

    feature: adicionando função subtração

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def subtracao(n1: int, n2: int) -> int:
+    """Subtrair um número por outro
+
+    Args:
+        n1: primeiro número.
+        n2: segundo número.
+
+    Returns:
+        A subtração dos números.
+    """
+    return n1 - n2
